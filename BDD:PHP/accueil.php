62fbdcab5c2367a5e450b0e8a0ceed20f6c2ca3e
Mike Gerlinger <bouarroudjmike@yahoo.fr>
2024-10-31 00:29:16 +0100

    ajoutaccueil

---
+++ BDD:PHP/accueil.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>  
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type= "text/css" href="accueilwondertravel.css">
+    
+    <title>Accueil</title>
+</head>
+   
+<body>
+
+<header>
+<h1> Wonder Travel </h1>
+<h2> Découvre le monde avec nous </h2>
+
+    <nav> 
+     <ul class="menu">
+         <li><a href="Mon compte">Mon Compte</a></li>
+         <li><a href="Publication">Publications</a></li>
+         <li><a href="Likes">Likes</a></li>
+         <li><a href="Commentaires">Commentaires</a></li>
+         <li><a href="Mes amis">Mes amis</a></li>
+    </ul>
+   </nav>
+</header>
+        
+
+</body>
+</html>
+
+
