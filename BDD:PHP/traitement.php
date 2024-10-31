83515c2a0a691e7997d112bd8a331b584fb9984f
Mike Gerlinger <bouarroudjmike@yahoo.fr>
2024-10-31 12:46:30 +0100

    modification du code en ajoutant sql_inscription et sql_utilisateur_id afin que la requete d'inscription apparaisse dans ma table utilisateurs

--- BDD:PHP/traitement.php
+++ BDD:PHP/traitement.php
@@ -17,6 +17,7 @@ if ($conn->connect_error) {
 // Vérifier si le formulaire a été envoyé
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Récupérer les valeurs du formulaire
+    
     $nom = $_POST['nom'];
     $prenom = $_POST['prenom'];
     $date_de_naissance = $_POST['date_de_naissance'];
@@ -26,27 +27,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Hachage du mot de passe pour plus de sécurité
     $mot_de_passe_hache = password_hash($mot_de_passe, PASSWORD_DEFAULT);
 
-    $utilisateur_id = $conn->insert_id; // Récupérer l'ID de l'utilisateur nouvellement créé
-
     // Préparer la requête d'insertion
-    $sql = "INSERT INTO Inscription (nom, prenom, date_de_naissance, email, mot_de_passe) 
-            VALUES ('$nom', '$prenom','$date_de_naissance', '$email', '$mot_de_passe_hache')";
+    $sql_utilisateur = "INSERT INTO Utilisateurs (nom, prenom, date_de_naissance, email, mot_de_passe) 
+                         VALUES ('$nom', '$prenom','$date_de_naissance', '$email', '$mot_de_passe_hache')";
 
     // Exécuter la requête et vérifier si elle a réussi
-    if ($conn->query($sql) === TRUE) {
+    if ($conn->query($sql_utilisateur) === TRUE) {
          // Récupérer l'ID de l'utilisateur inséré
          $utilisateur_id = $conn->insert_id;
+          // Insérer dans la table `Inscription` en utilisant `utilisateur_id`
+        $sql_inscription = "INSERT INTO Inscription (utilisateur_id, nom, prenom, date_de_naissance, email, mot_de_passe) 
+                            VALUES ('$utilisateur_id', '$nom', '$prenom', '$date_de_naissance', '$email', '$mot_de_passe_hache')";
+      if ($conn->query($sql_inscription) === TRUE) {
 
-         // Préparer la requête d'insertion dans la table inscription
-         $sql_inscription = "INSERT INTO inscription (utilisateur_id, nom, prenom, date_de_naissance, email, mot_de_passe) 
-                             VALUES ('$utilisateur_id', '$nom', '$prenom', '$date_de_naissance', '$email', '$mot_de_passe_hache')";
-        
+      }
         echo "Inscription réussie !";
     } else {
-        echo "Erreur : " . $sql . "<br>" . $conn->error;
+        echo "Erreur : " . $sql_inscription . "<br>" . $conn->error;
     }
 } else {
-    echo "Erreur : le formulaire n'a pas été envoyé correctement.";
+    echo  "Erreur : " . $sql_utilisateur . "<br>" . $conn->error;
 }
 
 // Fermer la connexion
